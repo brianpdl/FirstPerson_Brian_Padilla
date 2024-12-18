28d9aa4ceff8a1860966acb89f24352616f809fb
Brian <brianpsss21@gmail.com>
2024-12-18 17:37:37 +0100

    arreglos

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -14,7 +14,9 @@ public class Enemigo : MonoBehaviour
     private NavMeshAgent agent;
     private FirstPerson player;
 
-    private bool ventanaAbierta;
+
+    private Animator anim;
+    private bool ventanaAbierta = false;
     private bool puedoDanar = true;
 
     // Start is called before the first frame update
@@ -28,6 +30,38 @@ public class Enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //definir como destino la posicion del player.
         agent.SetDestination(player.transform.position);
+
+        // si la distancia que se queda hacia el objeto cae por debajo del stoppingDistance
+        if(agent.remainingDistance <= agent.stoppingDistance)
+        {
+            //me paro ante el 
+            agent.isStopped = true;
+            anim.SetBool("attacking", false);
+            
+            
+            //activar anim de ataque
+            //1.obtener el COMPONENTE ANIMATOR y almacenarlo en una variable
+            //2.a traves de esta variable buscar un metodo para ACTIVAR EL BOOL attacking
+        }
+    }
+
+    //Eventos de animacion
+
+    private void FinAtaque()
+    {
+        //caundo termina de atacar, vuelve a moverse
+        agent.isStopped = false;
+        anim.SetBool("attacking", false);
+    }
+
+    private void AbrirVentanaAtaque()
+    {
+        ventanaAbierta = true;
+    }
+    private void CerrarVentanaAtaque()
+    {
+        ventanaAbierta = false;
     }
 }
